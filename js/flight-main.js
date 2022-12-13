73a35f69efacdb2fcd3eecb33e0bfa6cd468642d
Saurabh Sharma <39699314+saurabhsharma2395@users.noreply.github.com>
2022-12-13 12:18:24 -0500

    deployment

---
+++ js/flight-main.js
@@ -0,0 +1,28 @@
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "nav ul") {
+    x.className += " responsive";
+  } else {
+    x.className = "nav ul";
+  }
+}
+
+const about = document.querySelector(".about");
+const btns = document.querySelectorAll(".tab-btn");
+const articles = document.querySelectorAll(".content");
+about.addEventListener("click", function (e) {
+  const id = e.target.dataset.id;
+  if (id) {
+    // remove selected from other buttons
+    btns.forEach(function (btn) {
+      btn.classList.remove("active");
+    });
+    e.target.classList.add("active");
+    // hide other articles
+    articles.forEach(function (article) {
+      article.classList.remove("active");
+    });
+    const element = document.getElementById(id);
+    element.classList.add("active");
+  }
+});
